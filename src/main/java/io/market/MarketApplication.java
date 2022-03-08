e52185eefb53ee77bc3201bdf0a92af67c58cbf0
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2022-03-07 21:24:01 -0500

    version 2

--- src/main/java/io/market/MarketApplication.java
+++ src/main/java/io/market/MarketApplication.java
@@ -4,10 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
@@ -16,14 +13,33 @@ public class MarketApplication {
     @Autowired
     public EmployeeRepository repository;
 
+    @Autowired
+    public MascotaRepository repositorym;
     @PostMapping("/employee")
     public Employee addEmployee(@RequestBody Employee employe){
         System.out.println(employe.toString());
         return repository.save(employe);
     }
+    @DeleteMapping("/employee")
+    public void removeEmployee(@RequestBody int id){
+        System.out.println("id: "+id);
+        repository.deleteById(id);
+    }
     @GetMapping("/employees")
     public List<Employee> getEmployees(){
         return repository.findAll();
+    }
+
+
+    @PostMapping("/mascota")
+    public Mascota addMascota(@RequestBody Mascota mascota){
+        System.out.println(mascota.toString());
+        System.out.println("--------------INSERT------------");
+        return repositorym.save(mascota);
+    }
+    @GetMapping("/mascotas")
+    public List<Mascota> getMascotas(){
+        return repositorym.findAll();
     }
     public static void main(String[] args) {
         SpringApplication.run(MarketApplication.class, args);
